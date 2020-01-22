82b2fd6d1da4c2e571aea6fb40bf5feae7e81bfc
marcioviegas <marcioaugustoviegas@gmail.com>
2020-01-21 22:51:45 -0300

    Add pagination to app

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-    <h1>Event Listing</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <template v-if="page != 1">
+      <router-link
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+        >Prev Page</router-link
+      >
+      |
+    </template>
+    <router-link :to="{ name: 'event-list', query: { page: page + 1 } }"
+      >Next Page</router-link
+    >
   </div>
 </template>
 
@@ -14,8 +24,16 @@ export default {
     EventCard
   },
   created() {
-    this.$store.dispatch('fetchEvents')
+    this.$store.dispatch('fetchEvents', {
+      perPage: 3,
+      page: this.page
+    })
   },
-  computed: mapState(['events'])
+  computed: {
+    page() {
+      return parseInt(this.$route.query.page) || 1
+    },
+    ...mapState(['events'])
+  }
 }
 </script>
